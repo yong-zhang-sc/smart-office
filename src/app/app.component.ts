5c49574b1b7c5cb433a507fd747a11108e8703b5
yong.zhang <yong.zhang@goodhopevisa.com>
2016-08-31 16:16:24 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {LoginComponent} from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule }   from '@angular/router';
+
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [LoginComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'app works!';
